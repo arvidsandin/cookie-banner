0506fb108d857477aaf20b764b5aa3f774393114
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-09-13 13:42:36 +0200

    feat: store selection in localStorage

--- src/components/ask-manager/ask-manager.spec.ts
+++ src/components/ask-manager/ask-manager.spec.ts
@@ -1,8 +1,39 @@
 import { AskManager } from './ask-manager';
 
 describe('my-component', () => {
-  it('initializes with a false', async () => {
+  it('initializes with correct default values', async () => {
     const theAskManager = new AskManager();
-    expect(theAskManager.hasAccepted).toBe(false);
+
+    expect(theAskManager.categories).toEqual([]);
+    expect(theAskManager.cookieConsent).toEqual({ lastAccepted: null, acceptedCategories: [] });
+  });
+
+  //short sleep to ensure different timestamps
+  function sleep() {
+    return new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  it('accept all and reject all changes relevant values', async () => {
+    const theAskManager = new AskManager();
+    var timeBefore = new Date();
+    await sleep();
+    theAskManager.categories = ['functional', 'analytical'];
+    theAskManager.acceptAllCookies();
+    await sleep();
+    var timeAfter = new Date();
+
+    expect(theAskManager.cookieConsent.acceptedCategories).toEqual(['functional', 'analytical']);
+    expect(theAskManager.cookieConsent.lastAccepted > timeBefore).toBeTruthy();
+    expect(theAskManager.cookieConsent.lastAccepted < timeAfter).toBeTruthy();
+
+    timeBefore = new Date();
+    await sleep();
+    theAskManager.rejectAllCookies();
+    await sleep();
+    timeAfter = new Date();
+
+    expect(theAskManager.cookieConsent.acceptedCategories).toEqual([]);
+    expect(theAskManager.cookieConsent.lastAccepted > timeBefore).toBeTruthy();
+    expect(theAskManager.cookieConsent.lastAccepted < timeAfter).toBeTruthy();
   });
 });
--- src/components/ask-manager/ask-manager.tsx
+++ src/components/ask-manager/ask-manager.tsx
@@ -7,9 +7,39 @@ import { Component, Prop, h } from '@stencil/core';
 })
 export class AskManager {
   /**
-   * Has the user accepted cookies
+   * list of categories of the cookies
    */
-  @Prop() hasAccepted: boolean = false;
+  @Prop() categories: string[] = [];
+
+  /**
+   * last time the privacy policy or which cookies that are used by the website was updated
+   * used to know if updated consent is needed
+   * can be any string that can be read by Date()
+   */
+  @Prop() cookiePolicyLastUpdated!: string;
+
+  cookieConsent = JSON.parse(localStorage.getItem('cookie-consent')) || {
+    lastAccepted: null,
+    acceptedCategories: [],
+  };
+
+  acceptCategories(categories: string[]) {
+    this.cookieConsent = {
+      lastAccepted: new Date(),
+      acceptedCategories: categories,
+    };
+    localStorage.setItem(`cookie-consent`, JSON.stringify(this.cookieConsent));
+  }
+
+  showOptions = () => {
+    // TODO
+  };
+  rejectAllCookies = () => {
+    this.acceptCategories([]);
+  };
+  acceptAllCookies = () => {
+    this.acceptCategories(this.categories);
+  };
 
   render() {
     return (
@@ -19,9 +49,9 @@ export class AskManager {
             This website uses cookies for functional, analytical and marketing purposes. Read more in our <a href="https://example.com">privacy policy</a> You can manage your
             choices at any time.
           </p>
-          <button>More options</button>
-          <button>Reject non-essential</button>
-          <button>Accept all</button>
+          <button onClick={this.showOptions}>More options</button>
+          <button onClick={this.acceptAllCookies}>Reject non-essential</button>
+          <button onClick={this.rejectAllCookies}>Accept all</button>
         </div>
       </div>
     );
