4aaa43907ef2de56b435a287560099ef6ca5f17b
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-09-13 09:28:36 +0200

    feat: add buttons+basic styling

---
+++ src/components/ask-manager/ask-manager.spec.ts
@@ -0,0 +1,8 @@
+import { AskManager } from './ask-manager';
+
+describe('my-component', () => {
+  it('initializes with a false', async () => {
+    const theAskManager = new AskManager();
+    expect(theAskManager.hasAccepted).toBe(false);
+  });
+});
--- src/components/ask-manager/ask-manager.tsx
+++ src/components/ask-manager/ask-manager.tsx
@@ -9,9 +9,21 @@ export class AskManager {
   /**
    * Has the user accepted cookies
    */
-  @Prop() hasAccepted: boolean;
+  @Prop() hasAccepted: boolean = false;
 
   render() {
-    return <div><p>This website uses cookies</p></div>;
+    return (
+      <div class="dimmable-background">
+        <div class="consent-box">
+          <p class="info-text">
+            This website uses cookies for functional, analytical and marketing purposes. Read more in our <a href="https://example.com">privacy policy</a> You can manage your
+            choices at any time.
+          </p>
+          <button>More options</button>
+          <button>Reject non-essential</button>
+          <button>Accept all</button>
+        </div>
+      </div>
+    );
   }
 }
