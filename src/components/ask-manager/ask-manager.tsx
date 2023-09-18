c79017520d4b565ba458fc52d780ceeaccd45c66
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-09-18 09:00:25 +0200

    feat: add view for more options

--- src/components/ask-manager/ask-manager.tsx
+++ src/components/ask-manager/ask-manager.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, State, h } from '@stencil/core';
 
 @Component({
   tag: 'ask-manager',
@@ -18,6 +18,8 @@ export class AskManager {
    */
   @Prop() cookiePolicyLastUpdated!: string;
 
+  @State() isInOptionsView: boolean = false;
+  optionsCheckboxes = [];
   cookieConsent = JSON.parse(localStorage.getItem('cookie-consent')) || {
     lastAccepted: null,
     acceptedCategories: [],
@@ -32,7 +34,10 @@ export class AskManager {
   }
 
   showOptions = () => {
-    // TODO
+    this.isInOptionsView = true;
+  };
+  hideOptions = () => {
+    this.isInOptionsView = false;
   };
   rejectAllCookies = () => {
     this.acceptCategories([]);
@@ -40,19 +45,50 @@ export class AskManager {
   acceptAllCookies = () => {
     this.acceptCategories(this.categories);
   };
+  acceptSelectedCookies = () => {
+    let selectedCategories = [];
+    for (const option of this.optionsCheckboxes) {
+      if (option.checked) {
+        selectedCategories.push(option.value);
+      }
+    }
+    this.acceptCategories(selectedCategories);
+    this.isInOptionsView = false;
+  };
 
   render() {
     return (
       <div class="dimmable-background">
-        <div class="consent-box">
-          <p class="info-text">
-            This website uses cookies for functional, analytical and marketing purposes. Read more in our <a href="https://example.com">privacy policy</a> You can manage your
-            choices at any time.
-          </p>
-          <button onClick={this.showOptions}>More options</button>
-          <button onClick={this.acceptAllCookies}>Reject non-essential</button>
-          <button onClick={this.rejectAllCookies}>Accept all</button>
-        </div>
+        {this.isInOptionsView ? (
+          <div class="options-box">
+            {this.categories.map(category => (
+              <div key={category}>
+                <input
+                  type="checkbox"
+                  class="checkbox"
+                  value={category}
+                  checked={this.cookieConsent.acceptedCategories.includes(category)}
+                  ref={element => {
+                    this.optionsCheckboxes[this.categories.indexOf(category)] = element;
+                  }}
+                ></input>
+                <p>{category}</p>
+              </div>
+            ))}
+            <button onClick={this.hideOptions}>Back</button>
+            <button onClick={this.acceptSelectedCookies}>Confirm selection</button>
+          </div>
+        ) : (
+          <div class="consent-box">
+            <p class="info-text">
+              This website uses cookies for functional, analytical and marketing purposes. Read more in our <a href="https://example.com">privacy policy</a> You can manage your
+              choices at any time.
+            </p>
+            <button onClick={this.showOptions}>More options</button>
+            <button onClick={this.acceptAllCookies}>Reject non-essential</button>
+            <button onClick={this.rejectAllCookies}>Accept all</button>
+          </div>
+        )}
       </div>
     );
   }
