691ec9567a95830928a78ffdda0dd24aaee2a8ca
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-10-04 19:21:20 +0200

    fix: solve Stencil error message

--- src/components/primary-banner/primary-banner.tsx
+++ src/components/primary-banner/primary-banner.tsx
@@ -19,10 +19,15 @@ export class PrimaryBanner {
     this.acceptCategories(state.options.categories.map(c => c.key));
   };
 
+  // Copy to not interfere with Stencil's renderer cache
+  private getCopyOfMainContent() {
+    return JSON.parse(JSON.stringify(state.options.texts.mainContent));
+  }
+
   render() {
     return (
       <div class="consent-box">
-        <p class="info-text">{state.options.texts.mainContent}</p>
+        <p class="info-text">{this.getCopyOfMainContent()}</p>
         <button onClick={this.showOptions}>{state.options.texts.moreOptions}</button>
         <button onClick={this.rejectAllCookies}>{state.options.texts.reject}</button>
         <button onClick={this.acceptAllCookies}>{state.options.texts.accept}</button>
